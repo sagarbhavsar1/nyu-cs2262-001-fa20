9c2fc97bb4e3db8433a29cd2bccbff117c127c7f
sagar bhavsar <70905948+sagarbhavsar1@users.noreply.github.com>
2024-02-19 19:40:28 -0500

    Update run.py

--- sample_time_app/run.py
+++ sample_time_app/run.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello world!'
+    
+@app.route('/time')
+def get_current_time():
+    # Function to return the current time
 
 
 app.run(host='0.0.0.0',
